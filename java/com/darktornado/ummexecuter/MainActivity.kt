3d5598c2624ee51b33358976f1effe1aba4ad22f
Dark Tornado <dt3141592@naver.com>
2022-02-11 23:22:01 +0900

    되돌리기/다시실행

--- java/com/darktornado/ummexecuter/MainActivity.kt
+++ java/com/darktornado/ummexecuter/MainActivity.kt
@@ -2,12 +2,34 @@ package com.darktornado.ummexecuter
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.ScrollView
+import android.widget.Toast
 
 class MainActivity : Activity() {
 
     var editor: CodeEditor? = null;
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            0 -> {
+
+            }
+            1 -> if (!editor!!.undo()) toast("더 이상 되돌릴 항목이 없어요.")
+            2 -> if (!editor!!.redo()) toast("더 이상 다시 실행할 항목이 없어요.")
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.add(0, 0, 0, "실행")
+        menu.add(0, 1, 0, "되돌리기")
+        menu.add(0, 2, 0, "다시 실행")
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +42,7 @@ class MainActivity : Activity() {
         scroll.addView(layout)
         setContentView(scroll)
     }
+
+    fun toast(msg: String) = Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 
 }
