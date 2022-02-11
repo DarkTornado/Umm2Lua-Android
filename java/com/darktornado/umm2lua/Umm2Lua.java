c9a44045b75bd6816e833d070a6c5edd5969468f
Dark Tornado <dt3141592@naver.com>
2022-02-11 22:34:16 +0900

    없어도 되는 부분 삭제

--- java/com/darktornado/umm2lua/Umm2Lua.java
+++ java/com/darktornado/umm2lua/Umm2Lua.java
@@ -8,23 +8,4 @@ public class Umm2Lua {
 		return cmp.execute();
 	}
 
-	public static void main(String[] args) {
-		String src = "어떻게\n\n" +
-				"엄.....\n" +
-				"엄어,\n" +
-				"식어!\n" +
-				"동탄어?준........\n" +
-				"준....\n" +
-				"어엄.......... ..........,,,\n" +
-				"식어어ㅋ\n" +
-				"어엄어어.\n" +
-				"식어어ㅋ\n" +
-				"\n이 사람이름이냐ㅋㅋ";
-		System.out.println("----- Before -----");
-		System.out.println(src);
-		System.out.println("----- After -----");
-		System.out.println(compile(src));
-		System.out.println("----- End -----");
-	}
-
 }
